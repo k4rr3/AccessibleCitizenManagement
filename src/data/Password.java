f4a6fcb9744210bb2221f7579d53a0a5fa0a54e1
k4rr3 <davidcarreras2003@gmail.com>
2023-12-10 14:24:52 +0100

    Password class and test added

--- src/data/Password.java
+++ src/data/Password.java
@@ -4,4 +4,35 @@ package data;
  * Essential data classes
  */
 public class Password {
+
+    //Assuming password cannot be changed => final
+    private final String password;
+
+    public Password(String password) {
+        if (isValidPassword(password)) {
+            this.password = password;
+        } else {
+            throw new IllegalArgumentException("Invalid password. Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit.");
+        }
+    }
+
+    private boolean isValidPassword(String password) {
+        //Password requirements:
+        //It must be at least 8 characters long.
+        //It must contain at least one uppercase letter, one lowercase letter, and one digit.
+
+        if (password.length() < 8) {
+            return false;
+        }
+
+        boolean hasUppercase = !password.equals(password.toLowerCase());
+        boolean hasLowercase = !password.equals(password.toUpperCase());
+        boolean hasDigit = password.matches(".*\\d.*");
+
+        return hasUppercase && hasLowercase && hasDigit;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
---
+++ test/PasswordTest.java
@@ -0,0 +1,32 @@
+import data.Password;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordTest {
+
+    @Test
+    void testValidPassword() {
+        Password password = new Password("Password1");
+        assertEquals("Password1", password.getPassword());
+    }
+
+    @Test
+    void testInvalidLength() {
+        assertThrows(IllegalArgumentException.class, () -> new Password("Pass1"));
+    }
+
+    @Test
+    void testNoUppercaseLetter() {
+        assertThrows(IllegalArgumentException.class, () -> new Password("password1"));
+    }
+
+    @Test
+    void testNoLowercaseLetter() {
+        assertThrows(IllegalArgumentException.class, () -> new Password("PASSWORD1"));
+    }
+
+    @Test
+    void testNoDigit() {
+        assertThrows(IllegalArgumentException.class, () -> new Password("Password"));
+    }
+}
